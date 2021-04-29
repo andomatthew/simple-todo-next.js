458f4f07dec0ceb6b8df6190da9275af9b4aee08
Alexandro Matthew <andomatthew@192.168.1.6>
2021-04-29 13:04:44 +0800

    refactor code on client pages api users api section

--- todo/pages/api/users/login.js
+++ todo/pages/api/users/login.js
@@ -2,19 +2,12 @@ import axios from 'axios'
 
 
 export default async function loginHandler(req, res) {
-  try {
-    console.log('masuk')
-    const databaseApi = 'http://localhost:3001'
-    const databaseApi2 = 'https://todo-next-js.herokuapp.com'
-    const { email, password } = req.body
-    const input = { email, password }
-    const response = await axios.post(`${databaseApi2}/login`, input) 
-    const token = response.data.accessToken
-    const { data } = await axios.get(`${databaseApi2}/users?email=${email}`)
-    const currentUser = data[0] 
-    res.status(response.status).json({ access_token: token, email: currentUser.email, id: currentUser.id })
-
-  } catch(err) {
-    res.status(400).json({ message: 'ups, error' })
-  }
+  const databaseApi = 'http://localhost:3001'
+  const { email, password } = req.body
+  const input = { email, password }
+  const response = await axios.post(`${databaseApi}/login`, input) 
+  const token = response.data.accessToken
+  const { data } = await axios.get(`${databaseApi}/users?email=${email}`)
+  const currentUser = data[0] 
+  res.status(response.status).json({ access_token: token, email: currentUser.email, id: currentUser.id })
 }
